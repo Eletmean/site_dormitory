ae061b5f682fadbc608b5e22fdbd40ad5a4d6ee4
Eletmean <elina.abbasova.11@gmail.com>
2024-12-18 01:58:17 +1000

    ...

--- your_application/database.py
+++ your_application/database.py
@@ -1,22 +1,14 @@
-# your_application/database.py
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+import pymysql  # Если вы используете pymysql
+#import mariadb  # Если вы используете mariadb
 
-# Замените на ваши данные
-DATABASE_URL = "mysql+pymysql://root:123@localhost/dbname"
+def get_connection():
+    # Параметры подключения
+    connection = pymysql.connect(
+        host='localhost',  # или IP-адрес вашего сервера
+        user='your_username',
+        password='your_password',
+        database='your_database'
+    )
+    return connection
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
 
-def init_db():
-    Base.metadata.create_all(bind=engine)
-
-# Функция для получения сессии
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
