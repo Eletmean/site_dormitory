2a190dd4ae10ec6a99259192aeac67e0bab066b2
Eletmean <elina.abbasova.11@gmail.com>
2024-12-18 01:08:06 +1000

    ...

--- your_application/app.py
+++ your_application/app.py
@@ -5,7 +5,3 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=10000)
