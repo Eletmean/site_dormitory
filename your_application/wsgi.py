fce31c05431c461e27c51443a7d98fb8e746367c
Eletmean <elina.abbasova.11@gmail.com>
2024-12-18 09:05:44 +1000

    ...

--- your_application/wsgi.py
+++ your_application/wsgi.py
@@ -1,7 +1,11 @@
+# your_application/wsgi.py
 from wsgiref.simple_server import make_server
-from your_application import router  # Импортируйте router из __init__.py
+from your_application.routes import router  # Импортируйте ваш router
+
+# Определите объект application
+application = router  # Это должен быть ваш WSGI-приложение
 
 if __name__ == "__main__":
-    httpd = make_server('', 8000, router)  # Используйте router здесь
+    httpd = make_server('', 8000, application)  # Используйте application здесь
     print("Serving on port 8000...")
     httpd.serve_forever()
