35e096eb610515e06cfea3e7ee8b8f76a71308e9
Eletmean <elina.abbasova.11@gmail.com>
2024-12-17 23:12:12 +1000

    ...

--- your_application/database.py
+++ your_application/database.py
@@ -1,9 +1,20 @@
 import pymysql  # Если вы используете pymysql
-# import mariadb  # Если вы используете mariadb
+import mariadb  # Если вы используете mariadb
 
 def get_connection():
     # Параметры подключения
     connection = pymysql.connect(
+        host='localhost',  # или IP-адрес вашего сервера
+        user='your_username',
+        password='your_password',
+        database='your_database'
+    )
+    return connection
+
+
+def get_connection():
+    # Параметры подключения
+    connection = mariadb.connect(
         host='localhost',  # или IP-адрес вашего сервера
         user='your_username',
         password='your_password',
