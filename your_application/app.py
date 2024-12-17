4c5ad3a14dda253cd9503b343164d4d9c1d41fc0
Eletmean <elina.abbasova.11@gmail.com>
2024-12-18 02:07:45 +1000

    ...

--- your_application/app.py
+++ your_application/app.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+def simple_app(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-type', 'text/plain; charset=utf-8')]
+    start_response(status, headers)
+    
+    return [b"Hello, World!"]
 
-app = FastAPI()
+# Запуск WSGI сервера
+from wsgiref.simple_server import make_server
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+httpd = make_server('', 8000, simple_app)
+print("Serving on port 8000...")
+httpd.serve_forever()
